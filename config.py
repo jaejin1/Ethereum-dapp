7da71f9d15ebf61013c9964d687cbbe703829bd0
jaajin1 <jaejin@icon.foundation>
2018-09-22 11:03:50 +0900

    add authentication

---
+++ config.py
@@ -0,0 +1,88 @@
+# -*- coding:utf-8 -*-
+
+import os
+import sys
+import time
+import pickle
+import random
+import numpy as np
+
+class_num = 2
+image_size = 32
+img_channels = 3
+
+test_num = 20000
+
+
+def prepare_data():
+    with open('data.pkl', 'rb') as f:
+        data = pickle.load(f)
+
+    with open('label_data.pkl', 'rb') as f:
+        label_data = pickle.load(f)
+
+    train_data, train_labels = data[:-test_num], label_data[:-test_num]
+    test_data, test_labels = data[-test_num:], label_data[-test_num:]
+
+    return train_data, train_labels, test_data, test_labels
+
+
+# ========================================================== #
+# ├─ _random_crop()
+# ├─ _random_flip_leftright()
+# ├─ data_augmentation()
+# └─ color_preprocessing()
+# ========================================================== #
+
+def _random_crop(batch, crop_shape, padding=None):
+    oshape = np.shape(batch[0])
+
+    if padding:
+        oshape = (oshape[0] + 2 * padding, oshape[1] + 2 * padding)
+    new_batch = []
+    npad = ((padding, padding), (padding, padding), (0, 0))
+    for i in range(len(batch)):
+        new_batch.append(batch[i])
+        if padding:
+            new_batch[i] = np.lib.pad(batch[i], pad_width=npad,
+                                      mode='constant', constant_values=0)
+        nh = random.randint(0, oshape[0] - crop_shape[0])
+        nw = random.randint(0, oshape[1] - crop_shape[1])
+        new_batch[i] = new_batch[i][nh:nh + crop_shape[0],
+                       nw:nw + crop_shape[1]]
+    return new_batch
+
+
+def _random_flip_leftright(batch):
+    for i in range(len(batch)):
+        if bool(random.getrandbits(1)):
+            batch[i] = np.fliplr(batch[i])
+    return batch
+
+
+def color_preprocessing(x_train, x_test):
+    x_train = x_train.astype('float32')
+    x_test = x_test.astype('float32')
+    x_train[:, :, :, 0] = (x_train[:, :, :, 0] - np.mean(x_train[:, :, :, 0])) / np.std(x_train[:, :, :, 0])
+    x_train[:, :, :, 1] = (x_train[:, :, :, 1] - np.mean(x_train[:, :, :, 1])) / np.std(x_train[:, :, :, 1])
+    x_train[:, :, :, 2] = (x_train[:, :, :, 2] - np.mean(x_train[:, :, :, 2])) / np.std(x_train[:, :, :, 2])
+
+    x_test[:, :, :, 0] = (x_test[:, :, :, 0] - np.mean(x_test[:, :, :, 0])) / np.std(x_test[:, :, :, 0])
+    x_test[:, :, :, 1] = (x_test[:, :, :, 1] - np.mean(x_test[:, :, :, 1])) / np.std(x_test[:, :, :, 1])
+    x_test[:, :, :, 2] = (x_test[:, :, :, 2] - np.mean(x_test[:, :, :, 2])) / np.std(x_test[:, :, :, 2])
+
+    return x_train, x_test
+
+def color_preprocessing_test(x_test):
+    x_test = x_test.astype('float32')
+
+    x_test[:, :, :, 0] = (x_test[:, :, :, 0] - np.mean(x_test[:, :, :, 0])) / np.std(x_test[:, :, :, 0])
+    x_test[:, :, :, 1] = (x_test[:, :, :, 1] - np.mean(x_test[:, :, :, 1])) / np.std(x_test[:, :, :, 1])
+    x_test[:, :, :, 2] = (x_test[:, :, :, 2] - np.mean(x_test[:, :, :, 2])) / np.std(x_test[:, :, :, 2])
+
+    return x_test
+
+def data_augmentation(batch):
+    batch = _random_flip_leftright(batch)
+    batch = _random_crop(batch, [32, 32], 4)
+    return batch
---
+++ test.py
@@ -0,0 +1,258 @@
+import tensorflow as tf
+from tflearn.layers.conv import global_avg_pool
+from tensorflow.contrib.layers import batch_norm, flatten
+from tensorflow.contrib.layers import xavier_initializer
+from tensorflow.contrib.framework import arg_scope
+from config import *
+import cv2
+import numpy as np
+import os
+
+# Hyperparameter
+growth_k = 24
+nb_block = 2  # how many (dense block + Transition Layer) ?
+init_learning_rate = 1e-4
+epsilon = 1e-4  # AdamOptimizer epsilon
+dropout_rate = 0.2
+
+# Momentum Optimizer will use
+nesterov_momentum = 0.9
+weight_decay = 1e-4
+
+# Label & batch_size
+batch_size = 16
+
+iteration = 782
+# batch_size * iteration = data_set_number
+
+test_iteration = 1
+
+total_epochs = 300
+
+
+def conv_layer(input, filter, kernel, stride=1, layer_name="conv"):
+    with tf.name_scope(layer_name):
+        network = tf.layers.conv2d(inputs=input, use_bias=False, filters=filter, kernel_size=kernel, strides=stride,
+                                   padding='SAME')
+        return network
+
+
+def Global_Average_Pooling(x, stride=1):
+    """
+    width = np.shape(x)[1]
+    height = np.shape(x)[2]
+    pool_size = [width, height]
+    return tf.layers.average_pooling2d(inputs=x, pool_size=pool_size, strides=stride) # The stride value does not matter
+    It is global average pooling without tflearn
+    """
+
+    return global_avg_pool(x, name='Global_avg_pooling')
+    # But maybe you need to install h5py and curses or not
+
+
+def Batch_Normalization(x, training, scope):
+    with arg_scope([batch_norm],
+                   scope=scope,
+                   updates_collections=None,
+                   decay=0.9,
+                   center=True,
+                   scale=True,
+                   zero_debias_moving_mean=True):
+        return tf.cond(training,
+                       lambda: batch_norm(inputs=x, is_training=training, reuse=None),
+                       lambda: batch_norm(inputs=x, is_training=training, reuse=True))
+
+
+def Drop_out(x, rate, training):
+    return tf.layers.dropout(inputs=x, rate=rate, training=training)
+
+
+def Relu(x):
+    return tf.nn.relu(x)
+
+
+def Average_pooling(x, pool_size=[2, 2], stride=2, padding='VALID'):
+    return tf.layers.average_pooling2d(inputs=x, pool_size=pool_size, strides=stride, padding=padding)
+
+
+def Max_Pooling(x, pool_size=[3, 3], stride=2, padding='VALID'):
+    return tf.layers.max_pooling2d(inputs=x, pool_size=pool_size, strides=stride, padding=padding)
+
+
+def Concatenation(layers):
+    return tf.concat(layers, axis=3)
+
+
+def Linear(x):
+    return tf.layers.dense(inputs=x, units=class_num, name='linear')
+
+def input_data():
+
+    image = cv2.imread('./jaejin.jpg')
+    image_resize = cv2.resize(image, (32,32))
+
+    data = np.array([image_resize])
+
+    data = color_preprocessing_test(data)
+    print('test_data load!!')
+    print(data.shape)
+
+    return data
+
+def Evaluate(sess):
+
+    for it in range(test_iteration):
+
+        test_dict = {
+            x: input_data(),
+            training_flag: False
+        }
+
+        result = sess.run(logits, feed_dict= test_dict)
+
+    return result
+
+
+class DenseNet():
+    def __init__(self, x, nb_blocks, filters, training):
+        self.nb_blocks = nb_blocks
+        self.filters = filters
+        self.training = training
+        self.model = self.Dense_net(x)
+
+    def bottleneck_layer(self, x, scope):
+        # print(x)
+        with tf.name_scope(scope):
+            x = Batch_Normalization(x, training=self.training, scope=scope + '_batch1')
+            x = Relu(x)
+            x = conv_layer(x, filter=4 * self.filters, kernel=[1, 1], layer_name=scope + '_conv1')
+            x = Drop_out(x, rate=dropout_rate, training=self.training)
+
+            x = Batch_Normalization(x, training=self.training, scope=scope + '_batch2')
+            x = Relu(x)
+            x = conv_layer(x, filter=self.filters, kernel=[3, 3], layer_name=scope + '_conv2')
+            x = Drop_out(x, rate=dropout_rate, training=self.training)
+
+            # print(x)
+
+            return x
+
+    def transition_layer(self, x, scope):
+        with tf.name_scope(scope):
+            x = Batch_Normalization(x, training=self.training, scope=scope + '_batch1')
+            x = Relu(x)
+            x = conv_layer(x, filter=self.filters, kernel=[1, 1], layer_name=scope + '_conv1')
+            x = Drop_out(x, rate=dropout_rate, training=self.training)
+            x = Average_pooling(x, pool_size=[2, 2], stride=2)
+
+            return x
+
+    def dense_block(self, input_x, nb_layers, layer_name):
+        with tf.name_scope(layer_name):
+            layers_concat = list()
+            layers_concat.append(input_x)
+
+            x = self.bottleneck_layer(input_x, scope=layer_name + '_bottleN_' + str(0))
+
+            layers_concat.append(x)
+
+            for i in range(nb_layers - 1):
+                x = Concatenation(layers_concat)
+                x = self.bottleneck_layer(x, scope=layer_name + '_bottleN_' + str(i + 1))
+                layers_concat.append(x)
+
+            x = Concatenation(layers_concat)
+
+            return x
+
+    def Dense_net(self, input_x):
+        x = conv_layer(input_x, filter=2 * self.filters, kernel=[7, 7], stride=2, layer_name='conv0')
+        # x = Max_Pooling(x, pool_size=[3,3], stride=2)
+
+
+        """
+        for i in range(self.nb_blocks) :
+            # 6 -> 12 -> 48
+            x = self.dense_block(input_x=x, nb_layers=4, layer_name='dense_'+str(i))
+            x = self.transition_layer(x, scope='trans_'+str(i))
+        """
+
+        x = self.dense_block(input_x=x, nb_layers=6, layer_name='dense_1')
+        x = self.transition_layer(x, scope='trans_1')
+
+        x = self.dense_block(input_x=x, nb_layers=12, layer_name='dense_2')
+        x = self.transition_layer(x, scope='trans_2')
+
+        x = self.dense_block(input_x=x, nb_layers=48, layer_name='dense_3')
+        x = self.transition_layer(x, scope='trans_3')
+
+        x = self.dense_block(input_x=x, nb_layers=32, layer_name='dense_final')
+
+        # 100 Layer
+        x = Batch_Normalization(x, training=self.training, scope='linear_batch')
+        x = Relu(x)
+        x = Global_Average_Pooling(x)
+        x = flatten(x)
+        x = Linear(x)
+
+        # x = tf.reshape(x, [-1, 10])
+        return x
+
+
+#train_x, train_y, test_x, test_y = prepare_data()
+#train_x, test_x = color_preprocessing(train_x, test_x)
+
+#print('========== train shape ==========')
+#print(train_x.shape)
+#print(train_y.shape)
+#print('========== test shape ==========')
+#print(test_x.shape)
+#print(test_y.shape)
+
+#print('========== data load success!! ==========')
+
+
+    # image_size = 32, img_channels = 3, class_num = 10
+x = tf.placeholder(tf.float32, shape=[None, image_size, image_size, img_channels])
+#label = tf.placeholder(tf.float32, shape=[None, class_num])
+
+training_flag = tf.placeholder(tf.bool)
+
+#learning_rate = tf.placeholder(tf.float32, name='learning_rate')
+
+logits = DenseNet(x=x, nb_blocks=nb_block, filters=growth_k, training=training_flag).model
+#cost = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(labels=label, logits=logits))
+
+
+
+#optimizer = tf.train.AdamOptimizer(learning_rate=learning_rate, epsilon=epsilon)
+#train = optimizer.minimize(cost)
+#correct_prediction = tf.equal(tf.argmax(logits, 1), tf.argmax(label, 1))
+#accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
+
+saver = tf.train.Saver(tf.global_variables())
+
+config = tf.ConfigProto(allow_soft_placement=True)
+
+with tf.Session(config=config) as sess:
+
+    ckpt = tf.train.get_checkpoint_state('./model')
+    if ckpt and tf.train.checkpoint_exists(ckpt.model_checkpoint_path):
+        saver.restore(sess, ckpt.model_checkpoint_path)
+    else:
+        sess.run(tf.global_variables_initializer())
+
+    epoch_learning_rate = init_learning_rate
+
+
+
+    modelName = "./saver/dense.ckpt"
+    saver.restore(sess, modelName)
+
+    print('model load')
+
+    result = Evaluate(sess)
+
+    print('==========')
+    print(result)
+    print('==========')
